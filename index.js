9dd5b8d77f22d5c8c4b610c05645d50696a5fc66
jordynh1 <jordynh124@gmail.com>
2021-03-20 14:36:34 +0000

    Tested JavaScript

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Hello World!");
+console.log("My name is Jordyn.");
+console.log("I am in the 9th grade.");
+
+function myFunction () {
+  console.log('We\'ll add our algorithm here!');
+};
